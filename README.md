7ca387c217868f30a5d89f81961741858c5e570e
MaidariTs <tmaidari@mail.ru>
2022-09-21 21:26:14 +0300

    1

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # sms_sender
+This little project can send sms message to your phone number
+
+Instruction:
+1. Clone and create a virtual environment
+    git clone
+    python -m venv venv
+    source venv/Scripts/activate
+
+2. Install Requirements:
+    pip install -r requirements.txt
+
+3. Register on the site 'https://www.twilio.com/'
+
+4. Create an '.env' file in the project directory and enter your data in it:
+    SID = ''
+    AUTH_TOKEN = ''
+    SENDER_PHONE = ''
+    RECEIVER_PHONE = ''
+
+5. It`s all
---
+++ main.py
@@ -0,0 +1,30 @@
+from twilio.rest import Client
+import os
+
+
+def sending_sms(text, receiver):
+
+    try:
+        account_sid = os.getenv('SID')
+        auth_token = os.getenv('AUTH_TOKEN')
+
+        client = Client(account_sid, auth_token)
+
+        client.messages.create(
+            body=text,
+            from_=os.getenv('SENDER_PHONE'),
+            to=receiver
+        )
+        return 'The message was successfully sent'
+
+    except Exception as ex:
+        return 'Something was wrong...', ex
+
+
+def main():
+    text = input('Please enter your message: ')
+    print(sending_sms(text=text, receiver=os.getenv('RECEIVER_PHONE')))
+
+
+if __name__ == '__main__':
+    main()
